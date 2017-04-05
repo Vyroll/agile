65d79f4876a080e980144f5ec05cf0f1870f51f0
Łukasz Nosal <lukasz94nosal@gmail.com>
2017-04-05 12:18:56 +0200

    created order views for admin and adjusted creating order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
   # GET /orders/1
   # GET /orders/1.json
   def show
+    @order_items = OrderItem.where(order_id: @order.id)
   end
 
   # GET /orders/new
@@ -64,11 +65,11 @@ class OrdersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
-      @order = Order.find(params[:id])
+       @order = Order.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:shipping_id, :status.name, :user.email)
+      params.require(:order).permit(:shipping_id, :order_status_id)
     end
 end
